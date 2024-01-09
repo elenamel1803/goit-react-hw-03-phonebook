c5972add4669fb99e8ad4c3c5fd7603ea956e4c6
Elena Melnyk <100163385+elenamel1803@users.noreply.github.com>
2024-01-09 16:55:58 +0200

    start

---
+++ src/components/App.jsx
@@ -0,0 +1,67 @@
+import { Component } from 'react';
+import ContactForm from './ContactForm/ContactForm';
+import Filter from './Filter/Filter';
+import ContactList from './ContactList/ContactList';
+
+class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+
+  addContact = newContact => {
+    // console.log(newContact);
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts, newContact],
+    }));
+  };
+
+  deleteContact = id => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== id),
+    }));
+  };
+
+  handleFilterChange = ({ target }) => {
+    this.setState({ filter: target.value });
+  };
+
+  filteredСontacts = () => {
+    const { contacts, filter } = this.state;
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log('componentDidUpdate');
+    const { contacts } = this.state;
+    if (contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(contacts));
+    }
+  }
+
+  render() {
+    const { contacts, filter } = this.state;
+    return (
+      <>
+        <h1>Phonebook</h1>
+        <ContactForm onAddContact={this.addContact} contacts={contacts} />
+
+        <h2>Contacts</h2>
+        <Filter onFilterChange={this.handleFilterChange} filter={filter} />
+        <ContactList
+          contacts={this.filteredСontacts()}
+          onDeleteContact={this.deleteContact}
+        />
+      </>
+    );
+  }
+}
+
+export default App;
