f93c3e301428947df7b77e8dafec89cd49365997
Elena Melnyk <100163385+elenamel1803@users.noreply.github.com>
2024-01-10 02:16:21 +0200

    update pattern in input

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -59,7 +59,7 @@ class ContactForm extends Component {
             value={name}
             required
             onChange={this.handleChange}
-            pattern="^[a-zA-Zа-яА-Я]+(([' \\-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
           />
         </div>
         <div>
@@ -70,7 +70,7 @@ class ContactForm extends Component {
             value={number}
             required
             onChange={this.handleChange}
-            pattern="\\+?\\d{1,4}?[ .\\-\\s]?\\(?\\d{1,3}?\\)?[ .\\-\\s]?\\d{1,4}[ .\\-\\s]?\\d{1,4}[ .\\-\\s]?\\d{1,9}"
+            pattern="\+?\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}"
           />
         </div>
 
